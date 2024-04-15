b4d1159bfc0027e3930157e6dcd68d581a2181ca
Saurav Sarkar <sauravsarkar260@gmail.com>
2024-04-15 15:43:50 +0600

    Contact

--- forms/contact.php
+++ forms/contact.php
@@ -1,6 +1,6 @@
 <?php
 
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'sauraavsarkar@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
