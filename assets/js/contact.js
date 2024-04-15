5747da8bf0f9e57afe99659ff2d15e4066e87c42
Saurav Sarkar <sauravsarkar260@gmail.com>
2024-04-16 01:06:36 +0600

    Check if any of the fields are empty

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,33 +1,31 @@
-
 function Send(){
-
-      
     var name = document.getElementById("Name").value;
     var email = document.getElementById("Email").value;
-
     var sub = document.getElementById("subject").value;
     var mess = document.getElementById("message").value;
 
+    // Check if any of the fields are empty
+    if (name === '' || email === '' || sub === '' || mess === '') {
+        swal("Error", "Please fill out all fields", "error");
+        return; // Stop execution if any field is empty
+    }
+
     var body = "Name: " + name + "<br/> Email:" + email + "<br/> Message:" + mess;
-     
-    console.log(body);
-     Email.send({
-      SecureToken : "5924ca43-f20b-4a91-89ec-f49a7031f540",
-      To : 'ovi.raj54@gmail.com',
-      From : "ovi.raj54@gmail.com",
-      Subject : sub,
-      Body : body
-     }).then(
-         message =>{
-             if(message=='OK'){
 
-                 swal("Successfull", "Your message Successfull Received", "success");
-             }
-             else{
-
-                 swal("Something Wrong", "Your Data is not Received", "error");
-             }
-         }
-       );
- }
-
+    // Proceed with sending the email if all fields are filled
+    Email.send({
+        SecureToken: "5924ca43-f20b-4a91-89ec-f49a7031f540",
+        To: 'ovi.raj54@gmail.com',
+        From: "ovi.raj54@gmail.com",
+        Subject: sub,
+        Body: body
+    }).then(
+        message => {
+            if (message === 'OK') {
+                swal("Success", "Your data has been successfully received", "success");
+            } else {
+                swal("Error", "Something went wrong. Your data was not received", "error");
+            }
+        }
+    );
+}
